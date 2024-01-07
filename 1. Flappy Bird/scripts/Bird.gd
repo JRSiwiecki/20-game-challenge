1d5100ebf9aac148711abf96d5dbf4c4f62c2c76
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-06 21:39:15 -0800

    make bird stationary

--- 1. Flappy Bird/scripts/Bird.gd
+++ 1. Flappy Bird/scripts/Bird.gd
@@ -1,12 +1,9 @@
 extends CharacterBody2D
 
-@export var bird_speed: float = 10.0
 @export var jump_height: float = -300.0
 @export var gravity: float = 980.0
 
 func _physics_process(delta: float) -> void:
-	velocity.x = bird_speed
-	
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = jump_height
 	
--- 1. Flappy Bird/scripts/Camera.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-@export var camera_speed: float = 10
-
-func _process(delta: float) -> void:
-	position.x += camera_speed * delta
