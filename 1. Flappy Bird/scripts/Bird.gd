2bfede3afbb4461318a86c3abf01fea13f5ad00e
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-06 21:18:59 -0800

    Fine tune gravity + jump values

--- 1. Flappy Bird/scripts/Bird.gd
+++ 1. Flappy Bird/scripts/Bird.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 @export var bird_speed: float = 10.0
-@export var jump_height: float = -100.0
-@export var gravity: float = 200.0
+@export var jump_height: float = -300.0
+@export var gravity: float = 980.0
 
 func _physics_process(delta: float) -> void:
 	velocity.x = bird_speed
