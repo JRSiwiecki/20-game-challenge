99668abb34cb349384a7effe631bf6b769b12eed
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-06 23:41:30 -0800

    Add saving + loading high score

--- 1. Flappy Bird/scripts/Game.gd
+++ 1. Flappy Bird/scripts/Game.gd
@@ -4,9 +4,13 @@ extends Node2D
 @export var min_gap: float = 250.0
 @export var max_gap: float = 350.0
 
-@onready var score_label: Label = $CanvasLayer/MarginContainer/Label
+@onready var score_label: Label = $"CanvasLayer/MarginContainer/Score Label"
+@onready var high_score_label: Label = $"CanvasLayer/MarginContainer/High Score Label"
+
 @onready var score_sound: AudioStreamPlayer = $"Score Scound"
 @onready var lose_sound: AudioStreamPlayer = $"Lose Sound"
+
+@onready var high_score: int = load_high_score()
 
 # Need this or we will have 2 score when passing through first pipe
 # and forever be ahead by 1 score.
@@ -17,8 +21,11 @@ var score: int = -1
 const SCREEN_WIDTH: float = 1152.0 + 200.0
 const SCREEN_HEIGHT: float = 646.0 / 2.0
 
+const SAVE_PATH: String = "user://savegame.save"
+
 func _ready() -> void:
 	spawn_pipes()
+	update_high_score_text()
 
 func _on_timer_timeout() -> void:
 	spawn_pipes()
@@ -56,9 +63,34 @@ func play_sound() -> void:
 	score_sound.play()
 
 func update_ui() -> void:
+	update_high_score_text()
+	update_score_text()
+
+func update_high_score_text() -> void:
+	high_score_label.text = "High Score: " + str(high_score)
+
+func update_score_text() -> void:
 	score_label.text = "Score: " + str(score)
 
 func end_game() -> void:
 	lose_sound.play()
+	await save_high_score(score)
 	await get_tree().create_timer(0.15).timeout
 	get_tree().quit()
+
+func save_high_score(current_score: int) -> void:
+	if score <= high_score:
+		return
+	
+	high_score = max(score, high_score)
+	
+	var save_game = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	save_game.store_var(high_score)
+
+func load_high_score():
+	# No save found
+	if not FileAccess.file_exists(SAVE_PATH):
+		return 0
+	
+	var save_game = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	return save_game.get_var(high_score)
