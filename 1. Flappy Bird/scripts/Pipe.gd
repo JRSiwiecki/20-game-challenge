b9e90c4f88e71909c5c650fd99ea78babf5778aa
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-06 22:03:04 -0800

    Delete pipes after offscreen

--- 1. Flappy Bird/scripts/Pipe.gd
+++ 1. Flappy Bird/scripts/Pipe.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	position.x += Globals.scroll_speed * delta
+	
+	if position.x <= -100.0:
+		queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Bird":
