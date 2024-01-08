76db88c29c491a7f31c14ff764e2d7682392207a
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-08 13:25:50 -0800

    Esc closes game

--- 2. Breakout/scripts/game.gd
+++ 2. Breakout/scripts/game.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @export var wall_hit : AudioStreamPlayer
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
 func _on_top_wall_body_entered(body: Node2D) -> void:
 	if body.name != "Ball": 
 		return
