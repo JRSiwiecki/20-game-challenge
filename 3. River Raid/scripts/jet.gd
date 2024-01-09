e5350d372c2ad00b581fe54e9e20e5da254c461c
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 02:45:47 -0800

    Add particle emitters to jet

--- 3. River Raid/scripts/jet.gd
+++ 3. River Raid/scripts/jet.gd
@@ -3,19 +3,28 @@ extends CharacterBody2D
 class_name Jet
 
 @export var animation : AnimatedSprite2D
+@export var left_particle_emitter : GPUParticles2D
+@export var right_particle_emitter : GPUParticles2D
+@export var engine_particle_emitter : GPUParticles2D
 
 var speed : float = Globals.scroll_speed
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0, -speed)
 	
 	if Input.is_action_pressed("forward"):
 		Globals.increase_scroll_speed()
+		left_particle_emitter.emitting = true
+		right_particle_emitter.emitting = true
+		engine_particle_emitter.emitting = true
 		velocity.y -= speed
 	
 	if Input.is_action_just_released("forward"):
 		Globals.reset_scroll_speed()
-	
+		left_particle_emitter.emitting = false
+		right_particle_emitter.emitting = false
+		engine_particle_emitter.emitting = false
+		
 	if Input.is_action_pressed("left"):
 		velocity.x += -speed
 		animation.play("lean_left")
@@ -25,6 +34,6 @@ func _physics_process(delta: float) -> void:
 		animation.play("lean_right")
 	
 	else:
-		animation.play("forward")
+		animation.play("default")
 	
 	move_and_slide()
