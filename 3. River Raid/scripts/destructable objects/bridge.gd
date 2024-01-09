e1e1bd9f11ec6a30887138596247f46346bdf4be
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 12:57:10 -0800

    Refactor file system, re-add bridge script

---
+++ 3. River Raid/scripts/destructable objects/bridge.gd
@@ -0,0 +1,10 @@
+extends StaticBody2D
+
+@export var health_component : HealthComponent
+
+# If body is not the missile, we don't care what collides with the bridge.
+func _on_hitbox_component_body_entered(body: Node2D) -> void:
+	if body.name != "Missile":
+		return
+	
+	health_component.damage()
