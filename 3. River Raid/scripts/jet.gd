0e0c66c49ad403a4d492e4a7a29d3df7c5009011
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 02:50:25 -0800

    Refactor more jet code, add braking

--- 3. River Raid/scripts/jet.gd
+++ 3. River Raid/scripts/jet.gd
@@ -12,6 +12,12 @@ var speed : float = Globals.scroll_speed
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0, -speed)
 	
+	check_forward_input()
+	check_side_input()
+	
+	move_and_slide()
+
+func check_forward_input() -> void:
 	if Input.is_action_pressed("forward"):
 		Globals.increase_scroll_speed()
 		turn_on_particle_emitters()
@@ -20,7 +26,15 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_released("forward"):
 		Globals.reset_scroll_speed()
 		turn_off_particle_emitters()
-		
+	
+	if Input.is_action_pressed("brake"):
+		Globals.decrease_scroll_speed()
+		velocity.y += speed
+	
+	if Input.is_action_just_released("brake"):
+		Globals.reset_scroll_speed()
+
+func check_side_input() -> void:
 	if Input.is_action_pressed("left"):
 		velocity.x += -speed
 		animation.play("lean_left")
@@ -31,8 +45,6 @@ func _physics_process(_delta: float) -> void:
 	
 	else:
 		animation.play("default")
-	
-	move_and_slide()
 
 func turn_on_particle_emitters() -> void:
 	left_particle_emitter.emitting = true
