19edccc80add7df785a81d408c6392ab8f2c3962
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 12:54:51 -0800

    Add jet fighter scene

---
+++ 3. River Raid/scripts/Fighter.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var health_component : HealthComponent
+
+@export var fighter_speed : float = 300.0
+
+func _physics_process(delta: float) -> void:
+	if position.x <= Globals.CAMERA_LEFT_LIMIT:
+		position.x = Globals.CAMERA_RIGHT_LIMIT
+	elif position.x >= Globals.CAMERA_RIGHT_LIMIT:
+		position.x = Globals.CAMERA_LEFT_LIMIT
+	
+	velocity.x = fighter_speed
+	move_and_slide()
+
+# If body is not the missile or jet, we don't care what collides with the fighter.
+func _on_hitbox_component_body_entered(body: Node2D) -> void:
+	if body.name != "Missile":
+		return
+	
+	health_component.damage()
--- 3. River Raid/scripts/globals.gd
+++ 3. River Raid/scripts/globals.gd
@@ -4,6 +4,9 @@ const INITIAL_SCROLL_SPEED : float = 400.0
 const MIN_SCROLL_SPEED : float = 300.0
 const MAX_SCROLL_SPEED : float = 500.0
 
+const CAMERA_LEFT_LIMIT : float = -640.0
+const CAMERA_RIGHT_LIMIT : float = 640.0
+
 var scroll_speed : float = 400.0
 
 func increase_scroll_speed() -> void:
