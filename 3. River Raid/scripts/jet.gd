de6aa8327ee09ccbc8a2846cff389ab835f8ae44
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 00:05:44 -0800

    Simplify animations for sake of my brain

--- 3. River Raid/scripts/jet.gd
+++ 3. River Raid/scripts/jet.gd
@@ -13,27 +13,13 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("left"):
 		velocity.x += -speed
-		
-		if animation.animation != "full_lean_left":
-			animation.play("lean_left")
+		animation.play("lean_left")
 	
 	elif Input.is_action_pressed("right"):
 		velocity.x += speed
-		
-		if animation.animation != "full_lean_right":
-			animation.play("lean_right")
+		animation.play("lean_right")
 	
 	else:
-		if animation.animation != "full_forward":
-			animation.play("forward")
+		animation.play("forward")
 	
 	move_and_slide()
-
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if animation.animation == "lean_left":
-		animation.play("full_lean_left")
-	elif animation.animation == "lean_right":
-		animation.play("full_lean_right")
-	elif animation.animation == "forward":
-		animation.play("full_forward")
