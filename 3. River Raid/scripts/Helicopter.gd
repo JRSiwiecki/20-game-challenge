4c998a065061147f42217338cae9d5ade2ff6d7d
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 12:33:16 -0800

    Add helicopter scene

--- 3. River Raid/scripts/Helicopter.gd
+++ 3. River Raid/scripts/Helicopter.gd
@@ -3,17 +3,17 @@ extends CharacterBody2D
 @export var health_component : HealthComponent
 @export var sprite : Sprite2D
 
-@export var boat_speed : float = 300.0
+@export var helicopter_speed : float = 400.0
 
 func _physics_process(_delta: float) -> void:
-	velocity.x = boat_speed
+	velocity.x = helicopter_speed
 	
 	move_and_slide()
 
-# If body is not the missile, we don't care what collides with the boat.
+# If body is not the missile, we don't care what collides with the helicopter.
 func _on_hitbox_component_body_entered(body: Node2D) -> void:
 	if body.name == "TileMap":
-		boat_speed = -boat_speed
+		helicopter_speed = -helicopter_speed
 		sprite.flip_h = !sprite.flip_h
 		
 	
