093ebcc187f5bc941cf714b67bf7d195c4eec552
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-09 12:26:27 -0800

    Add boat movement code

--- 3. River Raid/scripts/boat.gd
+++ 3. River Raid/scripts/boat.gd
@@ -1,9 +1,22 @@
 extends CharacterBody2D
 
 @export var health_component : HealthComponent
+@export var sprite : Sprite2D
+
+@export var boat_speed : float = 300.0
+
+func _physics_process(delta: float) -> void:
+	velocity.x = boat_speed
+	
+	move_and_slide()
 
 # If body is not the missile, we don't care what collides with the boat.
 func _on_hitbox_component_body_entered(body: Node2D) -> void:
+	if body.name == "TileMap":
+		boat_speed = -boat_speed
+		sprite.flip_h = !sprite.flip_h
+		
+	
 	if body.name != "Missile":
 		return
 	
